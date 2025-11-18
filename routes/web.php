af79c004454b7f2b272d0775771f458756ea17b0
ArfinWilly <arfinwilly5@gmail.com>
2025-11-18 22:59:07 +0700

    Add UI Files

--- routes/web.php
+++ routes/web.php
@@ -3,5 +3,8 @@
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('customer.menu');
 });
+Route::get('/cart', function () {
+    return view('customer.cart');
+})->name('cart');
