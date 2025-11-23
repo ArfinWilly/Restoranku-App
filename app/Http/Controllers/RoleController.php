7edf1a4072b4ec762d08d6b24640a70775fad240
ArfinWilly <arfinwilly5@gmail.com>
2025-11-23 20:20:58 +0700

    feat(admin/role): implement role management functionality (CRUD operations)

---
+++ app/Http/Controllers/RoleController.php
@@ -0,0 +1,93 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Role;
+use Illuminate\Http\Request;
+
+class RoleController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        $roles = Role::all() ;
+        return view('admin.role.index', compact('roles'));
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        return view('admin.role.create') ;
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        $validate = $request->validate([
+            'role_name' => 'required|string|max:255',
+            'description' => 'required|string',
+        ] , 
+        [
+            'role_name.required' => 'Nama role wajib diisi.',
+            'description.required' => 'Deskripsi role wajib diisi.',
+        ]);
+
+        Role::create($validate) ;
+
+        return redirect()->route('roles.index')->with('success', 'Data Role Berhasil Ditambahkan.') ;
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(string $id)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(string $id)
+    {
+        $role = Role::findOrFail($id) ;
+        return view('admin.role.edit', compact('role')) ;
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, string $id)
+    {
+        $validate = $request->validate([
+            'role_name' => 'required|string|max:255',
+            'description' => 'required|string',
+        ] , 
+        [
+            'role_name.required' => 'Nama role wajib diisi.',
+            'description.required' => 'Deskripsi role wajib diisi.',
+        ]);
+
+        $role = Role::findOrFail($id) ;
+        $role->update($validate) ;
+
+        return redirect()->route('roles.index')->with('success', 'Data Role Berhasil Diperbarui.') ;
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(string $id)
+    {
+        $role = Role::findOrFail($id) ;
+        $role->delete() ;
+
+        return redirect()->route('roles.index')->with('success', 'Data Role Berhasil Dihapus.') ;
+    }
+}
