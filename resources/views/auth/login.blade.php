7c7b8e7d7048fda5e2f57df1105f7c4f1309d9e8
ArfinWilly <arfinwilly5@gmail.com>
2025-12-02 11:21:26 +0700

    Add authentication and profile management features
    - Implemented input components for error messages, labels, buttons, and text inputs.
    - Created modal component for confirmation dialogs.
    - Developed navigation components for both desktop and mobile views.
    - Established layout files for application and guest views.
    - Added profile editing functionality with forms for updating profile information, changing passwords, and deleting accounts.
    - Set up routes for authentication, including registration, login, password reset, and email verification.
    - Created tests for authentication, email verification, password confirmation, and profile management.
    - Integrated Tailwind CSS for styling and responsive design.

---
+++ resources/views/auth/login.blade.php
@@ -0,0 +1,57 @@
+<!DOCTYPE html>
+<!-- saved from url=(0052)https://zuramai.github.io/mazer/demo/auth-login.html -->
+<html lang="en" data-bs-theme="light">
+
+<head>
+    <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Login - Mazer Admin Dashboard</title>
+    <link rel="stylesheet" crossorigin="" href="{{ asset('assets/admin/compiled/css/app.css') }}">
+    <link rel="stylesheet" crossorigin="" href="{{ asset('assets/admin/compiled/css/app-dark.css') }}">
+    <link rel="stylesheet" crossorigin="" href="{{ asset('assets/admin/compiled/css/auth.css') }}">
+</head>
+
+<body>
+    <script src="{{ asset('assets/admin/static/js/initTheme.js') }}"></script>
+    <div id="auth">
+
+        <div class="row h-100">
+            <div class="col-lg-5 col-12">
+                <div id="auth-left">
+                    <h1 class="auth-title">Log in Restoran-ku</h1>
+                    <p class="auth-subtitle mb-5">Silahkan masuk untuk mengelola restoran Anda</p>
+
+                    <form method="POST" action="{{ route('login') }}">
+                        @csrf
+                        <div class="form-group position-relative has-icon-left mb-4">
+                            <input type="email" class="form-control form-control-xl" placeholder="Email"
+                                name="email" :value="old('email')" required autofocus>
+                            <div class="form-control-icon">
+                                <i class="bi bi-person"></i>
+                            </div>
+                        </div>
+                        <div class="form-group position-relative has-icon-left mb-4">
+                            <input type="password" class="form-control form-control-xl" placeholder="Password"
+                                name="password" :value="old('password')" required autocomplete="current-password">
+                            <div class="form-control-icon">
+                                <i class="bi bi-shield-lock"></i>
+                            </div>
+                        </div>
+                        <button class="btn btn-primary btn-block btn-lg shadow-lg mt-5">Log in</button>
+                    </form>
+                </div>
+            </div>
+            <div class="col-lg-7 d-none d-lg-block">
+                <div id="auth-right">
+
+                </div>
+            </div>
+        </div>
+
+    </div>
+
+
+</body>
+
+</html>
--- routes/web.php
+++ routes/web.php
@@ -1,32 +1,21 @@
 <?php
 
+use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return redirect()->route('menu');
+    return view('welcome');
 });
 
-Route::get('/menu', [App\Http\Controllers\MenuController::class, 'index'])->name('menu');
+// Route::get('/dashboard', function () {
+//     return view('dashboard');
+// })->middleware(['auth', 'verified'])->name('dashboard');
 
-Route::get('/cart', [App\Http\Controllers\MenuController::class, 'cart'])->name('cart');
-Route::post('/cart/add', [App\Http\Controllers\MenuController::class, 'addCart'])->name('add.cart');
-Route::post('/cart/update', [App\Http\Controllers\MenuController::class, 'updateCart'])->name('update.cart');
-Route::post('/cart/remove', [App\Http\Controllers\MenuController::class, 'removeCart'])->name('remove.cart');
-Route::get('/cart/clear', [App\Http\Controllers\MenuController::class, 'clearCart'])->name('clear.cart');
+Route::middleware('auth')->group(function () {
+    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
+    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
+    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+});
 
-Route::get('/checkout' , [App\Http\Controllers\MenuController::class, 'checkout'])->name('checkout');
-Route::post('/checkout/store' , [App\Http\Controllers\MenuController::class, 'storeOrder'])->name('checkout.store');
-Route::get('/checkout/success/{orderId}' , [App\Http\Controllers\MenuController::class, 'checkoutSuccess'])->name('checkout.success');
-
-Route::get('/dashboard' , [App\Http\Controllers\DashboardController::class, 'index'])->name('dashboard');
-
-Route::resource('/categories' , App\Http\Controllers\CategoryController::class);
-
-Route::resource('/orders' , App\Http\Controllers\OrderController::class);
-
-Route::resource('/items' , App\Http\Controllers\ItemController::class);
-Route::post('/items/{id}/update-status', [App\Http\Controllers\ItemController::class, 'updateStatus'])->name('items.updateStatus');
-
-Route::resource('/roles' , App\Http\Controllers\RoleController::class);
-
-Route::resource('/users' , App\Http\Controllers\UserController::class);
+require __DIR__.'/auth.php';
+require __DIR__.'/app.php';
